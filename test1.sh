ab996133f2f1a66ed73f28149d2581418b82efa7
jemal <jemalmfth@gmail.com>
2018-12-17 14:07:32 -0500

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+jemaltest(){
+  echo "Test is passed for Jemal"
+}
+
+fer(){
+
+  echo "Test is passed for Feruz"
+}
+
 for arg in "$@"
 do
-
-if [ ${arg} -eq ${jemal} | ${moh} ]; then
-  echo "This is the new variable"
-else
-  echo "not correct"
-fi
+  if [ "$#" -eq 1 ] && [ "$arg" == "jemal" ]
+  then
+    #echo "Test is  passed for jemal"
+  jemaltest
+  elif [ "$#" -eq 2 ] && [ "$arg" == "Feruz" ]
+  then
+  fer
+  fi
+done
